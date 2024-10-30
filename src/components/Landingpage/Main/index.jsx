3fed70703e49de4128ead338879756f85d54410c
Sourya07 <singhsourya137@gmail.com>
2024-10-30 23:34:14 +0530

    adding Mobile view css

--- src/components/Landingpage/Main/index.jsx
+++ src/components/Landingpage/Main/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import "./styles.css"
 import Buttons from '../../Common/Button'
+import iphone from "../../../assets/iphone.png";
+import gradient from "../../../assets/gradient.png"
 function Main() {
     return (
         <div className='flex-info'>
             <div className='left-component'>
                 <h1 className='track-crypto-heading'> Track Crypto</h1>
                 <h1 className='real-time-heading'> Real Time. </h1>
-                <p className='info-text'>Stringx the best crypto tracker, see our coin </p>
+                <p className='info-text'>Stringx the best crypto tracker,watchlist the bitcoin and ethereum and you favoutite coin  </p>
 
                 <div className='btn-flex'>
                     <Buttons text={"Dashboard"}
@@ -18,7 +20,12 @@ function Main() {
                 </div>
             </div>
 
-            <div> phone</div>
+            <div className='phone-container'>
+                <img src={iphone} className='iphone' />
+                <img src={gradient}
+                    className='gradient' />
+
+            </div>
         </div>
     )
 }
