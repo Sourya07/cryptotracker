ea1197e773871068fd772d33ac30089725ecfcdb
Sourya07 <singhsourya137@gmail.com>
2024-11-27 23:02:57 +0530

    footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Header from '../components/Common/Header'
 import Main from '../components/Landingpage/Main'
 import Abes from '../components/Dashboard/Search'
+import Footer from '../components/Common/Footer'
 
 function Homepage() {
     return (
         <div>
             <Header />
             <Main />
-
+            <Footer />
 
         </div>
     )
